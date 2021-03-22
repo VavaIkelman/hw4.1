2e6bf9d8c2404eef9060f2a755a1ec024af7fc37
Вячеслав Илькевич <ivada@MacBook-Pro-Vaceslav.local>
2021-03-22 22:46:13 +0300

    Added all selects

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
-// TODO: write your code here
-import sum from './basic';
+export default function showCurrentHelth (persent) {
+    let healthType;
+    if (persent >= 50) healthType = "healthy";
+    else if (persent > 15 & persent < 50) healthType = "wounded";
+    else if (persent < 15) healthType = "critical";
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+    return healthType;
+}
