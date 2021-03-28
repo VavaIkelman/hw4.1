3f880ead39a31aa8d0db04d123473a9270848161
Вячеслав Илькевич <ivada@MacBook-Pro-Vaceslav.local>
2021-03-28 20:44:26 +0300

    Checked Eslint

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,46 +1,46 @@
-import showCurrentHelth from "../js/app.js";
+import showCurrentHelth from '../js/app';
 
-test ('check healthy', () => {
-const hero = {
+test('check healthy', () => {
+  const hero = {
     name: 'Маг',
     health: 90,
-}
+  };
 
-expect(showCurrentHelth(hero)).toBe('healthy');
+  expect(showCurrentHelth(hero)).toBe('healthy');
 });
 
-test ('check healthy', () => {
-    const hero = {
-        name: 'Воин',
-        health: 51,
-    }
-    
-    expect(showCurrentHelth(hero)).toBe('healthy');
+test('check healthy', () => {
+  const hero = {
+    name: 'Воин',
+    health: 51,
+  };
+
+  expect(showCurrentHelth(hero)).toBe('healthy');
 });
 
-test ('check wounded', () => {
-    const hero = {
-        name: 'Воин',
-        health: 49,
-    }
-    
-    expect(showCurrentHelth(hero)).toBe('wounded');
+test('check wounded', () => {
+  const hero = {
+    name: 'Воин',
+    health: 49,
+  };
+
+  expect(showCurrentHelth(hero)).toBe('wounded');
 });
 
-test ('check wounded', () => {
-    const hero = {
-        name: 'Воин',
-        health: 15,
-    }
-    
-    expect(showCurrentHelth(hero)).toBe('wounded');
+test('check wounded', () => {
+  const hero = {
+    name: 'Воин',
+    health: 15,
+  };
+
+  expect(showCurrentHelth(hero)).toBe('wounded');
 });
 
-test ('check critical', () => {
-    const hero = {
-        name: 'Воин',
-        health: 14,
-    }
-    
-    expect(showCurrentHelth(hero)).toBe('critical');
+test('check critical', () => {
+  const hero = {
+    name: 'Воин',
+    health: 14,
+  };
+
+  expect(showCurrentHelth(hero)).toBe('critical');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 export default function showCurrentHelth(hero) {
-    let currentHealth = '';
-    if (hero.health > 50){
-       currentHealth = 'healthy';
-    }else if (hero.health > 14 && hero.health < 51) {
-        currentHealth = 'wounded';
-    } else {
-        currentHealth = 'critical';
-    }
+  let currentHealth = '';
+  if (hero.health > 50) {
+    currentHealth = 'healthy';
+  } else if (hero.health > 14 && hero.health < 51) {
+    currentHealth = 'wounded';
+  } else {
+    currentHealth = 'critical';
+  }
 
-    return currentHealth;
+  return currentHealth;
 }
